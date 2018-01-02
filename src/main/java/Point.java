0e33aea498dd00aa35ea9c6e35936575c5979d4b
Vallery Ostanko <v.ostanko@quality-lab.ru>
2018-01-02 14:08:46 +0300

    rename class file

---
+++ src/main/java/Point.java
@@ -0,0 +1,17 @@
+public class Point {
+
+    int x;
+    int y;
+
+    public static double distance(Point p1, Point p2)
+    {
+        int deltaX = p1.x - p2.x;
+        int deltaY = p1.y - p2.y;
+
+        return Math.sqrt(deltaX*deltaX+deltaY*deltaY);
+    }
+
+}
+//Point point1;
+//Point point2;
+//Point.distance(point1,point2)
